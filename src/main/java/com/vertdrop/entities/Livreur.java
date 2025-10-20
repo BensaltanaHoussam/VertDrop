fdb688ed25e001fdcbc95609b91e4e3a8b8edfb4
BensaltanaHoussam <bensaltna07@gmail.com>
2025-10-20 14:47:32 +0100

    Add Livreur entity with attributes and relationships

---
+++ src/main/java/com/vertdrop/entities/Livreur.java
@@ -0,0 +1,54 @@
+package com.smartlogi.entities;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "livreurs")
+public class Livreur {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private String prenom;
+    private String vehicule;
+    private String telephone;
+
+    @OneToMany(mappedBy = "livreur", cascade = CascadeType.ALL)
+    private List<Colis> colis;
+
+    public Livreur() {}
+
+    public Livreur(String nom, String prenom, String vehicule, String telephone) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.vehicule = vehicule;
+        this.telephone = telephone;
+    }
+
+    // Getters and setters
+    public Long getId() { return id; }
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+    public String getPrenom() { return prenom; }
+    public void setPrenom(String prenom) { this.prenom = prenom; }
+    public String getVehicule() { return vehicule; }
+    public void setVehicule(String vehicule) { this.vehicule = vehicule; }
+    public String getTelephone() { return telephone; }
+    public void setTelephone(String telephone) { this.telephone = telephone; }
+    public List<Colis> getColis() { return colis; }
+    public void setColis(List<Colis> colis) { this.colis = colis; }
+
+    @Override
+    public String toString() {
+        return "Livreur{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", vehicule='" + vehicule + '\'' +
+                ", telephone='" + telephone + '\'' +
+                '}';
+    }
+}
