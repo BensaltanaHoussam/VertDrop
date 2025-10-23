7588460a85771d1ef04507351b72e19eeef63c9c
BensaltanaHoussam <bensaltna07@gmail.com>
2025-10-23 15:04:12 +0100

    Add CreateLivreurRequest and LivreurDto classes for managing delivery personnel data

---
+++ src/main/java/com/vertdrop/dto/LivreurDto.java
@@ -0,0 +1,49 @@
+package com.vertdrop.dto;
+
+public class LivreurDto {
+    private Long id;
+    private String nom;
+    private String prenom;
+    private String vehicule;
+    private String telephone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getVehicule() {
+        return vehicule;
+    }
+
+    public void setVehicule(String vehicule) {
+        this.vehicule = vehicule;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+}
